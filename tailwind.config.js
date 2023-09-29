1a17f5ebe787341cb16d9d139a104d9c60289ec1
Michael <14957047+ml7@users.noreply.github.com>
2023-09-29 12:23:17 -0700

    Add responsiveness

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,9 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    "./src/**/*.{html,js}",
-    "./src/*.{html,js}",
-    "./src/**/*"
+    "./**/*.{html,js}",
+    "./*.{html,js}",
+    "./**/*"
   ],
   theme: {
     extend: {},
